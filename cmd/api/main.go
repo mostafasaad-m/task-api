46c0637ec81d72bd7ec66a23abd417ea11c6f346
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-05 20:06:22 +0300

    health check 2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprint(w, `{"status":"ok"}`)
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health", healthHandler)
-
-	fmt.Println("Listening on 127.0.0.1:9000")
-
-	err := http.ListenAndServe("127.0.0.1:9000", mux)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("CI/CD Works!")
 }
