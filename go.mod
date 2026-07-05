6aca808e079bc5acdd31470f746e1e393c299103
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-05 20:48:13 +0300

    env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mostafasaad-m/task-api
 
 go 1.26.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -17,6 +19,7 @@ type Config struct {
 }
 
 func Load() (*Config, error) {
+	_ = godotenv.Load()
 	cfg := &Config{
 		AppPort:    getEnv("APP_PORT", "9000"),
 		DBHost:     getEnv("DB_HOST", "localhost"),
