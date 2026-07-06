d1ae423167eec7511f55408a3c13d48d14666dd1
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-06 12:35:44 +0300

    connect db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/mostafasaad-m/task-api/internal/config"
 	"github.com/mostafasaad-m/task-api/internal/server"
+	"github.com/mostafasaad-m/task-api/internal/store"
 )
 
 func main() {
+	fmt.Println("Hello World")
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	db, err := store.Connect(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close(context.Background())
 
 	srv := server.New(cfg)
 
