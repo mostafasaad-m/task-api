979a4552634ef48a63d2cd2dcdf9012883fb3fa6
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-07 12:10:05 +0300

    test:manual add user test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"context"
-	"fmt"
+
 	"log"
 
 	"github.com/mostafasaad-m/task-api/internal/config"
+	"github.com/mostafasaad-m/task-api/internal/models"
+	"github.com/mostafasaad-m/task-api/internal/repository"
 	"github.com/mostafasaad-m/task-api/internal/server"
 	"github.com/mostafasaad-m/task-api/internal/store"
 )
 
 func main() {
-	fmt.Println("ci/cd last check")
+	//fmt.Println("ci/cd last check")
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	db, err := store.Connect(cfg)
+	repo := repository.NewUserRepository(db)
+
+	user := &models.User{
+		Username:     "mostafa",
+		Email:        "mostafa@example.com",
+		PasswordHash: "not_hashed_yet",
+	}
+
+	if err := repo.Create(user); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Created user with ID %d", user.ID)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,4 +44,5 @@ func main() {
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
 	}
+
 }
