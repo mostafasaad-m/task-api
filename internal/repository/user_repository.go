f34b5c793db43a09140503c0a7d3ab035ec5fe3c
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-07 12:13:11 +0300

    test:manual add user test

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,15 +1,39 @@
 package repository
 
 import (
+	"context"
+
 	"github.com/jackc/pgx/v5"
+
+	"github.com/mostafasaad-m/task-api/internal/models"
 )
 
 type UserRepository struct {
-	db *pgx.Conn // database dependency injection
+	db *pgx.Conn //database dependency injection
 }
 
 func NewUserRepository(db *pgx.Conn) *UserRepository {
 	return &UserRepository{
 		db: db,
 	}
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+	query := `
+		INSERT INTO users (username, email, password_hash)
+		VALUES ($1, $2, $3)
+		RETURNING id, created_at, updated_at;
+	`
+
+	return r.db.QueryRow(
+		context.Background(),
+		query,
+		user.Username,
+		user.Email,
+		user.PasswordHash,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
 }
