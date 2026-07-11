cb8db130be4fbd66258fa727c8e7a7aeecc812e6
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-11 12:45:22 +0300

    fix: main error

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-
 	"log"
 
 	"github.com/mostafasaad-m/task-api/internal/config"
 	"github.com/mostafasaad-m/task-api/internal/handlers"
 	"github.com/mostafasaad-m/task-api/internal/repository"
-
 	"github.com/mostafasaad-m/task-api/internal/server"
 	"github.com/mostafasaad-m/task-api/internal/store"
 )
