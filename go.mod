895c00ab905bed5889df10a513fe9dabb2d9b8d8
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-13 14:33:52 +0300

    Feat: Password hashing with bcrypt

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.10.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	golang.org/x/crypto v0.54.0 // indirect
+	golang.org/x/text v0.40.0 // indirect
 )
--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/mostafasaad-m/task-api/internal/models"
 	"github.com/mostafasaad-m/task-api/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandler struct {
@@ -27,17 +28,36 @@ type RegisterRequest struct {
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	var req RegisterRequest
 
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if req.Username == "" || req.Email == "" || req.Password == "" {
+		http.Error(w, "all fields are required", http.StatusBadRequest)
+		return
+	}
+
+	//TODO: strings.TrimSpace()
+
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword(
+		[]byte(req.Password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		http.Error(w, "failed to hash password", http.StatusInternalServerError)
 		return
 	}
 
 	user := &models.User{
 		Username:     req.Username,
 		Email:        req.Email,
-		PasswordHash: req.Password, // we'll hash it next
+		PasswordHash: string(hashedPassword),
 	}
-
 	if err := h.users.Create(user); err != nil {
 		http.Error(w, "failed to create user", http.StatusInternalServerError)
 		return
@@ -46,9 +66,11 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	json.NewEncoder(w).Encode(map[string]any{
+	if err := json.NewEncoder(w).Encode(map[string]any{
 		"id":       user.ID,
 		"username": user.Username,
 		"email":    user.Email,
-	})
+	}); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+	}
 }
